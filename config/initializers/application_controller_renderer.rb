e22c9c19a8f36329c9d1d2737df366ea3c680cc9
岸颯人 <hayatokishi@MacBook-Pro.local>
2019-04-14 17:23:40 +0900

    [Finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '2775a69d28e3ede1d2238311266a4874bdbc918d154c87850cc1abbbad63487884b459e32d3450ea05635c9d123cf74008de010d142c8dda98c91fd159b8d7a0'
